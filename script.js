5f21bca92bbd848d02dc2110488e53e5116ab9d9
Chandilene <chandileneborges@gmail.com>
2026-02-27 10:03:40 -0300

    feat: logica melhorada com for loop

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-"use_strict";
 const inputNumber = document.querySelector("#number");
 const inputFrom = document.querySelector("#from");
 const inputTo = document.querySelector("#to");
-const permissionToRepeat = document.querySelector("#permission_to_repeat");
+const noRepeatPermission = document.querySelector("#noRepeatPermission");
 
 const button_draw = document.querySelector("#draw");
 
 const showResultDiv = document.querySelector(".showResult");
 
-function sortearNumeros(quantity, min, max, permissionRepeat) {
+/**
+ *
+ * @param {number} quantity
+ * @param {number} min
+ * @param {number} max
+ * @param {boolean} noRepeat
+ * @returns {number[]}
+ */
+function sortearNumeros(quantity, min, max, noRepeat) {
   let drawnNumbers = [];
   let interval = max - min + 1;
 
-  if (!permissionRepeat && quantity > interval) {
+  if (noRepeat && quantity > interval) {
     alert("Erro: quantidade de números maior que o intervalo disponível.");
   }
 
-  while (drawnNumbers.length < quantity) {
+  for (let i = 0; i < quantity; ) {
     let numero = Math.floor(Math.random() * interval) + min;
 
-    if (permissionRepeat) {
+    if (!noRepeat) {
       drawnNumbers.push(numero);
+      i++;
     } else {
       if (!drawnNumbers.includes(numero)) {
         drawnNumbers.push(numero);
+        i++;
       }
     }
   }
@@ -35,13 +44,12 @@ button_draw.addEventListener("click", () => {
   const qtdNumbers = parseInt(inputNumber.value);
   const qtdMin = parseInt(inputFrom.value);
   const qdtMax = parseInt(inputTo.value);
-  const noRepeat = permissionToRepeat.checked;
+  const noRepeat = noRepeatPermission.checked;
+  console.log(noRepeat);
 
-  const result = sortearNumeros(qtdNumbers, qtdMin, qdtMax, !noRepeat);
+  const result = sortearNumeros(qtdNumbers, qtdMin, qdtMax, noRepeat);
 
   const showResultParagraph = document.createElement("p");
   showResultParagraph.innerHTML = `Números sorteados: ${result.join("-")}`;
   showResultDiv.append(showResultParagraph);
-
-  console.log("Sorteados:", result);
 });
